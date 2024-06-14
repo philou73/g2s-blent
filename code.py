525f6958efc0c04db7818c3666485e98c7e64800
Blent User <user@blent>
2024-06-14 12:48:04 +0000

    Travail sur branche refonte

--- code.py
+++ code.py
@@ -1,3 +1,3 @@
 from datetime import datetime
 
-print("Hello ! Il est {}.".format(datetime.now().strftime("%H:%M:%S")))
+print("Hello frient ! Il est {}.".format(datetime.now().strftime("%H:%M:%S")))
