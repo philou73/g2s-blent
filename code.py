2a108287078d83c2258df9d79556c3ac7c613638
Blent User <user@blent>
2024-06-14 12:50:54 +0000

    Modificatoin de code.py

--- code.py
+++ code.py
@@ -1,3 +1,3 @@
 from datetime import datetime
 
-print("Hello ! Il est {}.".format(datetime.now().strftime("%H:%M:%S")))
+print("Hello Blent ! Il est {}.".format(datetime.now().strftime("%H:%M:%S")))
